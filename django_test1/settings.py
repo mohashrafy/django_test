fb673c47e565fd7f68084274e7a609e91645b27a
Mohammad Ashrafy <87927586+mohashrafy@users.noreply.github.com>
2022-08-13 00:54:53 -0700

    Update settings.py
    hiding the important codes

--- django_test1/settings.py
+++ django_test1/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^s_!ohx7y4j+ww(6ac&rzkg5u=6_+rlz)o^dvbfj&-96k-d_h='
+# SECURITY WARNING: keep the secret key used in production secret! It must be added before the final run of the project
+SECRET_KEY = ' ' # intentionaly left blank
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
